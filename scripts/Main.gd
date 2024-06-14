6322653911d7eace9227a4e9348f75a26cdb772d
MikkoVasankari <vasankarimikko@gmail.com>
2024-06-14 17:09:28 +0300

    All latest without extra code

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,7 +1,5 @@
 extends Node
 
-@export var player_size: Vector2i = Vector2i(32, 32) # Should be the size of your character sprite, or slightly bigger
-
 @onready var _MainWindow: Window = get_window()
 @onready var _SubWindow: Window = $Window
 
