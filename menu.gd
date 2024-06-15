e157a78454baa08d65ff556d6cfcc926cc33b7aa
Jonne Aspinen <bgo957@myy.haaga-helia.com>
2024-06-15 14:36:04 +0300

    menu and stuff

---
+++ menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://options.tscn")
+
+func _on_exit_button_pressed():
+	get_tree().quit()
