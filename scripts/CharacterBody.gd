0c341bfe5c1b69fa493d980b0b72e900942742ee
Jonne Aspinen <bgo957@myy.haaga-helia.com>
2024-06-16 15:43:12 +0300

    cycling animation and sound

--- scripts/CharacterBody.gd
+++ scripts/CharacterBody.gd
@@ -3,25 +3,29 @@ extends CharacterBody2D
 @export var speed = 280
 
 @onready var timer: Timer = $Timer
-@onready var Sprite = $Sprite2D
+@onready var Sprite = $AnimatedSprite2D
 @onready var monkeySound = $MonkeySound
+@onready var cycleSound = $CycleSound
 @onready var banana_1 = $"../../banana1"
 @onready var banana_2 = $"../../banana2"
 @onready var banana_count_label = $Camera2D/Banana_count_label
 @export var banana_count = 0
 
+
 func _ready():
 	# Assuming the Timer node is a child of the current node
 	timer.one_shot = true
 
+
 func reset_speed():
 	speed = 280
+
 
 func collect_banana():
 	banana_count += 1
 	var bananas_collected = "Bananas collected: %s"
 	banana_count_label.text = bananas_collected % banana_count
-	
+
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
@@ -36,27 +40,45 @@ func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_E:
 			monkeySound.play(0)
-		
+
+
 func _physics_process(delta):
+	
+	var cycling = false
 	
 	if Input.is_action_pressed("ui_right"):
 		# Move as long as the key/button is pressed.
 		position.x += speed * delta
 		Sprite.flip_h = false
+		Sprite.play("cycle_anim")
+		cycleSound.play(2.85)
+		cycling = true
 		
 	if Input.is_action_pressed("ui_left"):
 		# Move as long as the key/button is pressed.
 		position.x -= speed * delta
 		Sprite.flip_h = true
-			
+		Sprite.play("cycle_anim")
+		cycleSound.play(2.85)
+		cycling = true
+		
 	if Input.is_action_pressed("ui_up"):
 		position.y -= speed * delta
+		Sprite.play("cycle_anim")
+		cycleSound.play(2.85)
+		cycling = true
 		
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta
-	
+		Sprite.play("cycle_anim")
+		cycleSound.play(2.85)
+		cycling = true
+		
+	if !cycling:
+		Sprite.stop()
+		
 	move_and_slide()
-	
+
 
 func _on_banana_1_body_entered(_body):
 	banana_1.visible = false
